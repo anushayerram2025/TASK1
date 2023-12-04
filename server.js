e6ec1f94d03707aa6039bef1dab7af891a8f1b6f
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2023-12-04 21:20:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+/*register api which lets user to register himself using email and password
+Login using email and password
+Send Forgot password (as of now send email otp in api response because cannot integrate emailing as of now)
+Verify forgot password otp and update password which came in request body
+Get all user list
+Get a user details based on given id
+Get my details which return details of logged in user
+Update user details
+Delete user (soft delete)*/
+
+const express=require('express');
+const Model = require('./Model');
+
+
+const app=express();
+app.set('view engine','ejs');
+app.get('/register',function(req,res){
+        /*rendering the resgister page*/
+        res.render('register')
+})
+
+app.post('/save',async function(req,res){
+    /*save data from req to database*/
+    const user= await Model.create({
+        Name:req.name,
+        Username:req.username,
+        password:req.password,
+        phone_no:req.phone_no
+    })
+    user.save();
+    res.redirect('/login');
+})
+app.get('/login',function(req,res){
+    res.render('login')
+})
+app.post('/login',function(req,res){
+    /*autherntication*/
+    /*password hashing adn checking*/
+    const user=Model.find({Username:req.username,password:req.password});
+    if (!user){
+        return res.render('error',{
+            error:'Invalid Username or Password'
+        }) 
+    }
+    res.redirect('/profile/:username')/* only after authentication")*/
+})
+app.get('/profile/:username',async function(req,res){
+    const user=await Model.find({Username:req.params.username});
+    console.log(user);
+    res.send('LOGGED IN');
+}) 
+app.post('/forgot',function(req,res){
+
+
+    res.render('forgot',{password:34567});
+})
+app.post('/passUpdate',async function(req,res){
+    console.log(req.body)
+    const {user_name,pass,otp}=req.body;
+    const  user= await Model.findOneAndUpdate({Username:user_name},{password:pass});
+    console.log(user);
+    res.redirect('/login');
+})
+app.listen(3000);
+
